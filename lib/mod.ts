14de7d13e9dfe78b65dd0a41af9b6ca11fa16bc6
Luca Casonato <hello@lcas.dev>
2023-10-26 16:44:24 +0200

    display fn in wasm

--- lib/mod.ts
+++ lib/mod.ts
@@ -1,9 +1,28 @@
-import { instantiate } from "./v8_valueserializer_wasm.generated.js";
+import {
+  DisplayFormat,
+  instantiate,
+} from "./v8_valueserializer_wasm.generated.js";
 
-const { deserialize: deserialize_ } = await instantiate();
+const { display: display_ } = instantiate();
 
 // deno-lint-ignore no-explicit-any
 export function deserialize(bytes: Uint8Array): any {
-  const init = deserialize_(bytes);
+  const init = display_(bytes, DisplayFormat.Eval);
   return eval(init);
+}
+
+export function display(
+  bytes: Uint8Array,
+  format: "repl" | "eval" | "expression" = "repl",
+) {
+  const format_ = format === "repl"
+    ? DisplayFormat.Repl
+    : format === "eval"
+    ? DisplayFormat.Eval
+    : format === "expression"
+    ? DisplayFormat.Expression
+    : (() => {
+      throw new Error("Invalid format: " + format);
+    })();
+  return display_(bytes, format_);
 }
--- wasm/lib.rs
+++ wasm/lib.rs
@@ -1,18 +1,33 @@
-use v8_valueserializer::display;
-use v8_valueserializer::DisplayFormat;
 use v8_valueserializer::DisplayOptions;
 use v8_valueserializer::ValueDeserializer;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn deserialize(bytes: Vec<u8>) -> Result<String, JsError> {
+#[repr(u8)]
+pub enum DisplayFormat {
+  Repl = 0,
+  Expression = 1,
+  Eval = 2,
+}
+
+#[wasm_bindgen]
+pub fn display(
+  bytes: Vec<u8>,
+  format: DisplayFormat,
+) -> Result<String, JsError> {
   let deserializer = ValueDeserializer::default();
   let (value, heap) = deserializer.read(&bytes)?;
-  let str = display(
+  let str = v8_valueserializer::display(
     &heap,
     &value,
     DisplayOptions {
-      format: DisplayFormat::Eval,
+      format: match format {
+        DisplayFormat::Repl => v8_valueserializer::DisplayFormat::Repl,
+        DisplayFormat::Expression => {
+          v8_valueserializer::DisplayFormat::Expression
+        }
+        DisplayFormat::Eval => v8_valueserializer::DisplayFormat::Eval,
+      },
     },
   );
   Ok(str)
